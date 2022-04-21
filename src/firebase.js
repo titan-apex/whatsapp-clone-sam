342f27bc179b52e540fe0354e99ed6ba64a71021
titan-apex <samkr554@gmail.com>
2022-04-21 20:04:13 +0530

    npm and firebase fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  // your firebase config
+  apiKey: "AIzaSyBeiQ6bqNWTQ8qfymlAx_2uRh9K1dWI38A",
+  authDomain: "whatsapp-stripped-348aa.firebaseapp.com",
+  projectId: "whatsapp-stripped-348aa",
+  storageBucket: "whatsapp-stripped-348aa.appspot.com",
+  messagingSenderId: "563230176189",
+  appId: "1:563230176189:web:da0dc6d8811386c4b1d8d4",
 };
 
 firebase.initializeApp(firebaseConfig);
